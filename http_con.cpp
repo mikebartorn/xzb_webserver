de72aa7e862bca89cdd0bcea75ffe47875c1715e
xzb <1318270748@.com>
2023-04-04 05:26:07 +0800

    1

--- http_con.cpp
+++ http_con.cpp
@@ -18,7 +18,7 @@ const char* error_500_title = "Internal Error";
 const char* error_500_form = "There was an unusual problem serving the requested file.\n";
 
 // 网站的根目录
-const char* doc_root = "/home/xzb/code/resources";
+const char* doc_root = "/home/xzb/code/xzb_webserver/resources";
 
 //构造函数
 http_con::http_con(){
